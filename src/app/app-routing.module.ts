1e4dee31778feb41193aff6ab6a1059d23169650
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2021-07-26 00:57:35 -0400

    ✨ Configure project routing
    Add the Fontawesome and Bootstrap CDN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CreateStudentComponent } from './components/admin/create-student/create-student.component';
+import { ListStudentsComponent } from './components/admin/list-students/list-students.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'list-students', pathMatch: 'full' },
+  { path: 'list-students', component: ListStudentsComponent },
+  { path: 'create-student', component: CreateStudentComponent },
+  { path: 'edit-student/:id', component: CreateStudentComponent },
+  { path: '**', redirectTo: 'list-students', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
