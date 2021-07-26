c5fb94ee7349efa37b008531cd14ead3a4ffac0b
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2021-07-26 01:01:41 -0400

    ✨ Add CRUD methods in student service
    - add
    - get
    - delete
    - update

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  addStudent(student: any): Promise<any> {
+    return this.firestore.collection('students').add(student);
+  }
+
+  getStudents(): Observable<any> {
+    return this.firestore
+      .collection('students', (ref) => ref.orderBy('creationDate', 'asc'))
+      .snapshotChanges();
+  }
+
+  deleteStudent(id: string): Promise<any> {
+    return this.firestore.collection('students').doc(id).delete();
+  }
+
+  getStudent(id: string): Observable<any> {
+    return this.firestore.collection('students').doc(id).snapshotChanges();
+  }
+
+  updateStudent(id: string, data: any): Promise<any> {
+    return this.firestore.collection('students').doc(id).update(data);
+  }
 }
