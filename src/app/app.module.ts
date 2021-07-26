e652387096faba69878f5a83915435fd3e66a711
jrodriguezdiazz <jrodriguezdiazz@outlook.com>
2021-07-26 00:48:10 -0400

    🌱 Basic components and services were created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateStudentComponent } from './components/admin/create-student/create-student.component';
+import { ListStudentsComponent } from './components/admin/list-students/list-students.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateStudentComponent,
+    ListStudentsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
